f79c20dab8b29ebe834deab06e196da2312a8857
Ildar Usmanov <wajox@mail.ru>
2018-01-30 16:07:15 +0300

    update token provider test add clean up method

--- services/token_provider.go
+++ services/token_provider.go
@@ -1,15 +1,75 @@
 package services
 
-type TokenProvider struct{}
+import (
+    "time"
+    "sync"
+    "github.com/google/uuid"
+)
 
-func CreateTokenProvider() *TokenProvider {
-    return &TokenProvider{}
+type Token struct {
+    Timestamp int64
+    Value string
 }
 
-func Generate() string {
-    return "new-token"
+func CreateToken() *Token {
+    tokenValue := uuid.New().URN()
+    timestamp := time.Now().Unix()
+
+    return &Token{
+        Timestamp: timestamp,
+        Value: tokenValue,
+    }
 }
 
-func Validate(token string) bool {
-    return "new-token" == token
+type TokenProvider struct {
+    sync.Mutex
+    tokenLifetime int64
+    tokens map[string]*Token
+}
+
+func CreateTokenProvider(tokenLifetime int64) *TokenProvider {
+    return &TokenProvider{
+        tokenLifetime: tokenLifetime,
+        tokens: make(map[string]*Token),
+    }
+}
+
+func (p *TokenProvider) Generate() string {
+    newToken := p.createNewToken()
+
+    return newToken.Value
+}
+
+func (p* TokenProvider) Validate(tokenValue string) bool {
+    return p.tokenExists(tokenValue)
+}
+
+func (p *TokenProvider) CleanUp() {
+    for tokenValue, token := range p.tokens {
+        if p.isExpired(token.Timestamp) {
+            delete(p.tokens, tokenValue)
+        }
+    }
+}
+
+func (p *TokenProvider) isExpired(tokenTime int64) bool {
+    return time.Now().Unix() > tokenTime + p.tokenLifetime
+}
+
+func (p *TokenProvider) tokenExists(tokenValue string) bool {
+    p.Lock()
+    _, ok := p.tokens[tokenValue]
+    p.Unlock()
+
+    return ok
+}
+
+func (p *TokenProvider) createNewToken() *Token {
+    newToken := CreateToken()
+
+    p.Lock()
+    p.tokens[newToken.Value] = newToken
+    p.Unlock()
+
+    return newToken
 }
--- services/token_provider_test.go
+++ services/token_provider_test.go
@@ -1,18 +1,21 @@
 package services
 
 import (
+  "time"
   "testing"
   "github.com/stretchr/testify/assert"
 )
 
+var tokenLifetime = int64(3600)
+
 func TestCreateTokenProvider(t *testing.T) {
-    provider := CreateTokenProvider()
+    provider := CreateTokenProvider(tokenLifetime)
 
     assert.NotNil(t, provider)
 }
 
 func TestGenerate(t *testing.T) {
-    provider := CreateTokenProvider()
+    provider := CreateTokenProvider(tokenLifetime)
 
     newToken := provider.Generate()
 
@@ -25,11 +28,27 @@ func TestGenerate(t *testing.T) {
 }
 
 func TestValidate(t *testing.T) {
-    provider := CreateTokenProvider()
+    provider := CreateTokenProvider(tokenLifetime)
     validToken := provider.Generate()
     invalidToken := validToken + "1"
 
     assert := assert.New(t)
     assert.True(provider.Validate(validToken))
     assert.False(provider.Validate(invalidToken))
+}
+
+func TestCleanUp(t *testing.T) {
+    var (
+        minTokenLifetime = int64(3)
+    )
+
+    provider := CreateTokenProvider(minTokenLifetime)
+    expiredToken := provider.Generate()
+    time.Sleep(time.Duration(minTokenLifetime+1) * time.Second)
+    provider.CleanUp()
+    newToken := provider.Generate()
+
+    assert := assert.New(t)
+    assert.False(provider.Validate(expiredToken))
+    assert.True(provider.Validate(newToken))
 }
